5f1e6ac48f3f2599d877f5835f38760441345150
Aakash Kumar <aakash_kumar1@softprodigy.com>
2022-12-21 16:14:30 +0530

    ..

--- app_Jsonkeyvalueupdate/urls.py
+++ app_Jsonkeyvalueupdate/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from app_Jsonkeyvalueupdate.views import UpdateJSON
+from app_Jsonkeyvalueupdate.views import CalculateValue, UpdateJSON, download
 
 urlpatterns = [
-    path('', UpdateJSON.as_view(), name='test'),
+    path('', UpdateJSON.as_view(), name='Edit Value'),
+    path('Calculate', CalculateValue.as_view(), name='Calculate Value'),
+    path('download/<str:path>', download, name='download'),
 ]
--- app_Jsonkeyvalueupdate/views.py
+++ app_Jsonkeyvalueupdate/views.py
@@ -1,13 +1,12 @@
 import os
 import json
-import mimetypes
-
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.views import View
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
-from django.http.response import HttpResponse
-
+import subprocess
+from subprocess import Popen
+from django.http.response import HttpResponse, Http404
 
 class UpdateJSON(View):
     
@@ -67,3 +66,155 @@ class UpdateJSON(View):
 
         except Exception as e:
             return render(request, 'update_json.html', {'error': 'error: '+str(e)})
+
+def json_extract(obj, key):
+    arr = []
+
+    def extract(obj, arr, key):
+        if isinstance(obj, dict):
+            for k, v in obj.items():
+                if isinstance(v, (dict, list)):
+                    extract(v, arr, key)
+                elif k == key:
+                    arr.append(v)
+        elif isinstance(obj, list):
+            for item in obj:
+                extract(item, arr, key)
+        return arr
+
+    values = extract(obj, arr, key)
+    return values
+
+def update_data(data, updated_data):
+    if type(data) is dict:
+        for k, v in data.items():
+            if k in updated_data:
+                for k1, v1 in updated_data.items():
+                    if k1 in data:
+                        if type(data[k1]) != dict:
+                            data[k1] = v1
+            else:
+                update_data(v, updated_data)
+    elif type(data) is list:
+        for i in data:
+            update_data(i, updated_data)
+
+    return data
+import re
+class CalculateValue(View):
+
+    def get(self, request):
+        return render(request, 'calculate_json.html')
+
+    def post(self, request):
+        action = request.POST.get('action', 0)
+        if action == '1':
+            try:
+                temp_file = request.FILES['json_file']
+                file_content = temp_file.read()
+                file_data = json.loads(file_content)
+                data  = file_data
+                block_data = file_data['merge']
+                s = ''
+                for data1 in block_data:
+                    value = str(data1['replace'])
+                    if value.isnumeric():
+                        value
+                    else:
+                        value = '"' + value + '"'
+                    s += data1['find'] + ' = ' + value + '\n'
+                data = {"block_data":s,"filename":temp_file.name}
+                data['data'] = file_content.decode('utf-8')
+                return render(request, 'calculate_json.html', context=data)
+            except Exception as e:
+                return render(request, 'calculate_json.html', {'error': str(e)})
+
+        elif action == '3':
+            try:
+                filename = request.POST.get('filename')
+                script_block = request.POST.get('script_block')
+
+                with open(os.path.join(settings.MEDIA_ROOT, filename), 'r') as f:
+                    show_data = json.loads(f.read())
+
+                if not temp_keys.strip():
+                    return render(request, 'calculate_json.html', {
+                        'filename': filename,
+                        'keys_to_update': ', '.join(script_block),
+                        'show_data': json.dumps(show_data, indent=4),
+                        'script_block': script_block,
+                        'error': 'please add atleast 1 key to update.'
+                    })
+
+                temp_keys = [i.strip() for i in temp_keys.split(',') if i]
+                keys_to_update = []
+                for i in temp_keys:
+                    i = i.strip()
+                    if '=' in i:
+                        f = '==' in i or '!=' in i or '>=' in i or '<=' in i or '!=' in i or 'in' in i or 'not' in i
+                        if not f:
+                            keys_to_update.append(i)
+
+                temp = script_block.split("\n")
+                l = [i.split("=")[0].strip().replace(" ", "") for i in temp if i and "=" in i]
+                s = set([i for i in l if i.isalpha() or i.isalnum()])
+
+                script_name = os.path.join(settings.MEDIA_ROOT, filename + '_script.py')
+                script_name = script_name.replace('(', '').replace(')', '').replace(' ', '')
+
+                with open(script_name, 'w') as f:
+                    f.write(script_block)
+
+                    dict_str = '\ndata = {'
+                    for i in s:
+                        dict_str += f'"{i}": {i}, '
+                    dict_str += '}\n'
+
+                    f.write(dict_str)
+                    f.write('\nimport json')
+                    f.write('\nprint(json.dumps(data))\n')
+
+                # this will run the shell command `cat me` and capture stdout and stderr
+                proc = Popen(["python3", script_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                # this will wait for the process to finish.
+                proc.wait()
+                read_data = proc.stdout.read().decode()
+                read_err = proc.stderr.read().decode()
+                if read_err:
+                    context = {
+                        'filename': filename,
+                        'show_data': {},
+                        'keys_to_update': ', '.join(temp_keys),
+                        'script_block': script_block,
+                        'error': read_err
+                    }
+                    return render(request, 'calculate_json.html', context)
+                else:
+                    updated_data = json.loads(read_data)
+
+                show_data = update_data(data=show_data, updated_data=updated_data)
+
+                with open(os.path.join(settings.MEDIA_ROOT, filename), 'w') as f:
+                    f.write(json.dumps(show_data, indent=4))
+
+                context = {'filename': filename, 'show_data': json.dumps(show_data, indent=4), 'keys_to_update': ', '.join(temp_keys), 'script_block': script_block}
+                return render(request, 'calculate_json.html', context)
+            except IsADirectoryError as e:
+                return render(request, 'calculate_json.html', {'error': 'Please upload a file before proceed!'})
+            except Exception as e:
+                return render(request, 'calculate_json.html', {'error': e})
+
+        return render(request, 'calculate_json.html', {'error': 'Invalid action!'})
+
+
+def download(request, path):
+    try:
+        file_path = os.path.join(settings.MEDIA_ROOT, path)
+        if os.path.exists(file_path):
+            with open(file_path, 'rb') as fh:
+                response = HttpResponse(fh.read(), content_type="application/force-download")  # content_type="application/json")
+                response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+                return response
+    except:
+        pass
+    raise Http404
